5fd0d5277e02b2a00984032937aee16588c09228
Mohamed Diaa <mohamed.diaa.elsayed0@gmail.com>
2025-08-30 16:18:25 +0300

    Add Seeder for app

---
+++ FoodDonationSystem.API/Seed/DataSeeder.cs
@@ -0,0 +1,22 @@
+﻿using FoodDonationSystem.Core.Entities;
+using FoodDonationSystem.Data.Context;
+using Microsoft.AspNetCore.Identity;
+
+namespace FoodDonationSystem.API.Seed
+{
+    public static class DataSeeder
+    {
+        public static async Task SeedRoles(IServiceProvider serviceProvider)
+        {
+            using var scope = serviceProvider.CreateScope();
+
+            var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+            var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<ApplicationRole>>();
+
+            await context.Database.EnsureCreatedAsync();
+
+            await RoleSeeder.SeedAsync(roleManager);
+        }
+    }
+}
---
+++ FoodDonationSystem.API/Seed/RoleSeeder.cs
@@ -0,0 +1,28 @@
+﻿using FoodDonationSystem.Core.Entities;
+using Microsoft.AspNetCore.Identity;
+
+namespace FoodDonationSystem.API.Seed
+{
+    public static class RoleSeeder
+    {
+        public static async Task SeedAsync(RoleManager<ApplicationRole> roleManager)
+        {
+            var roles = new[]
+             {
+                new ApplicationRole("Admin") { Description = "System Administrator" },
+                new ApplicationRole("Restaurant") { Description = "Restaurant Owner/Manager" },
+                new ApplicationRole("Charity") { Description = "Charity Organization" },
+                new ApplicationRole("Volunteer") { Description = "Volunteer Driver" },
+                new ApplicationRole("Individual") { Description = "Individual Donor" }
+            };
+
+            foreach (var role in roles)
+            {
+                if (!await roleManager.RoleExistsAsync(role.Name))
+                {
+                    await roleManager.CreateAsync(new ApplicationRole(role.Name));
+                }
+            }
+        }
+    }
+}
