880aed62832eca4b7aed8bc9affc428578c64688
Mohamed Diaa <mohamed.diaa.elsayed0@gmail.com>
2025-08-30 18:51:15 +0300

    Update Program..cs With Extenion Methods

--- FoodDonationSystem.API/Program.cs
+++ FoodDonationSystem.API/Program.cs
@@ -1,60 +1,11 @@
 
-using FoodDonationSystem.API.Seed;
-using FoodDonationSystem.Core.Entities;
-using FoodDonationSystem.Data.Context;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
+using FoodDonationSystem.API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddAllServices(builder.Configuration);
 
-builder.Services.AddControllers();
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-
-builder.Services.AddIdentity<ApplicationUser, ApplicationRole>(options =>
-{
-
-    options.Password.RequireDigit = true;
-    options.Password.RequiredLength = 6;
-    options.Password.RequireNonAlphanumeric = false;
-    options.Password.RequireUppercase = false;
-    options.Password.RequireLowercase = false;
-
-
-    options.User.RequireUniqueEmail = true;
-
-
-    options.SignIn.RequireConfirmedEmail = false;
-    options.SignIn.RequireConfirmedPhoneNumber = false;
-})
-.AddEntityFrameworkStores<ApplicationDbContext>()
-.AddDefaultTokenProviders();
-
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowAll",
-        builder =>
-        {
-            builder.AllowAnyOrigin()
-                   .AllowAnyMethod()
-                   .AllowAnyHeader();
-        });
-});
 
 var app = builder.Build();
 
-app.UseHttpsRedirection();
-
-app.UseCors("AllowAll");
-
-app.UseAuthentication();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.MapGet("/", () => "Hello World!");
-
-await DataSeeder.SeedRoles(app.Services);
+await app.ConfigurePipelineAsync();
 app.Run();
