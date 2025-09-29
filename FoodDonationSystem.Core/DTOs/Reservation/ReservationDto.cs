609531bf3ce8c793aae04fce4357ef37b0bf60ce
Mohamed Diaa <mohamed.diaa.elsayed0@gmail.com>
2025-09-29 15:34:12 +0300

    (refactor):Update Reervation dto and add Description for Reservation Status

--- FoodDonationSystem.Core/DTOs/Reservation/ReservationDto.cs
+++ FoodDonationSystem.Core/DTOs/Reservation/ReservationDto.cs
@@ -1,27 +1,29 @@
 using FoodDonationSystem.Core.Enums;
+using FoodDonationSystem.Core.Extensions;
 
 namespace FoodDonationSystem.Core.DTOs.Reservation
 {
-	public class ReservationDto
-	{
-		public int Id { get; set; }
-		public DateTime ReservationTime { get; set; }
-		public ReservationStatus Status { get; set; }
-		public string? Notes { get; set; }
-		public DateTime? PickupTime { get; set; }
-		public string? PickupPersonName { get; set; }
-		public string? PickupPersonPhone { get; set; }
+    public class ReservationDto
+    {
+        public int Id { get; set; }
+        public DateTime ReservationTime { get; set; }
+        public ReservationStatus Status { get; set; }
+        public string StatusDisplayName => Status.ToDisplayName();
+        public string? Notes { get; set; }
+        public DateTime? PickupTime { get; set; }
+        public string? PickupPersonName { get; set; }
+        public string? PickupPersonPhone { get; set; }
 
-		// Links
-		public int DonationId { get; set; }
-		public int CharityId { get; set; }
+        // Links
+        public int DonationId { get; set; }
+        public int CharityId { get; set; }
 
-		// Projections for convenience
-		public string DonationFoodType { get; set; } = string.Empty;
-		public DateTime DonationExpiry { get; set; }
-		public int RestaurantId { get; set; }
-		public string RestaurantName { get; set; } = string.Empty;
-		public string RestaurantPhone { get; set; } = string.Empty;
-		public string RestaurantAddress { get; set; } = string.Empty;
-	}
+        // Projections for convenience
+        public string DonationFoodType { get; set; } = string.Empty;
+        public DateTime DonationExpiry { get; set; }
+        public int RestaurantId { get; set; }
+        public string RestaurantName { get; set; } = string.Empty;
+        public string RestaurantPhone { get; set; } = string.Empty;
+        public string RestaurantAddress { get; set; } = string.Empty;
+    }
 }
--- FoodDonationSystem.Core/Enums/ReservationStatus.cs
+++ FoodDonationSystem.Core/Enums/ReservationStatus.cs
@@ -1,10 +1,16 @@
-﻿namespace FoodDonationSystem.Core.Enums
+﻿using System.ComponentModel;
+
+namespace FoodDonationSystem.Core.Enums
 {
     public enum ReservationStatus
     {
+        [Description("معلق")]
         Pending = 1,
+        [Description("مؤكد")]
         Confirmed = 2,
+        [Description("مكتمل")]
         Completed = 3,
+        [Description("ملغي")]
         Cancelled = 4,
     }
 }
